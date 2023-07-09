35ae171f63c9f3e384e83fe55a4efdc30340c62b
erfanara <m.erfann.ara@gmail.com>
2023-07-09 10:07:15 +0330

    [add test] add 2 noob tests

---
+++ sample/test1.c
@@ -0,0 +1,19 @@
+
+int func(void)
+{
+  int retval = 0;
+  for (int i = 0; i<10; i++) {
+    for (int j =0 ;j<10;j++) {
+      for (int k =0 ; k<10; k++) {
+        printf("okeb");
+      }
+    }
+  }
+  return retval;
+}
+
+int main(int argc, char *argv[])
+{
+
+  func();
+}
---
+++ sample/test2.c
@@ -0,0 +1,15 @@
+
+
+
+int test1(char t[10])
+{
+  printf("salam");
+  return 0;
+}
+
+int main(int argc, char *argv[])
+{
+  test1("123");
+
+  return 0;
+}
